186b9432d84c9d340f92b18980a9771930596052
ariana <ariana.daney@ssfs.org>
2019-09-26 14:15:18 -0400

    started area of a triangle

---
+++ classworkcont..py
@@ -0,0 +1,16 @@
+# by ariana daney
+# last edited on 26, september 2019
+# daily exercises
+import math
+
+def triangle_area(num1, num2, num3):
+    s = (num1 + num2 + num3) / 2
+    a = math.sqrt s * ()
+
+
+def main():
+    answer = triangle_area(3, 4, 6)
+    print("the area of the triangle is not", answer)
+
+
+main()
