2554ce15ff43a4f79f79400cc078f422a5fec690
ariana <ariana.daney@ssfs.org>
2019-09-26 13:49:47 -0400

    created an adding funtion

--- classwork.py
+++ classwork.py
@@ -2,10 +2,12 @@
 # last modified 25, 2019
 # daily exercises
 
+
 def make_top_of_a_hexagon():
     print("  ________ ")
     print(" /        \\")
     print("/          \\")
+
 
 def make_bottom_of_a_hexagon():
     print("\\          /")
@@ -24,18 +26,24 @@ def happy_birthday(name):
 
 
 def adding_numbers(number1, number2):
-    print(number1 + number2)
+    print(number1, "+", number2, "=", number1 + number2)
 
-make_top_of_a_hexagon()
-make_bottom_of_a_hexagon()
-make_quotes()
-make_top_of_a_hexagon()
-make_bottom_of_a_hexagon()
-make_quotes()
-make_bottom_of_a_hexagon()
-make_top_of_a_hexagon()
-make_quotes()
-make_bottom_of_a_hexagon()
-happy_birthday("Liana and Allison")
 
-adding_numbers(2,4)
+def main():
+    make_top_of_a_hexagon()
+    make_bottom_of_a_hexagon()
+    make_quotes()
+    make_top_of_a_hexagon()
+    make_bottom_of_a_hexagon()
+    make_quotes()
+    make_bottom_of_a_hexagon()
+    make_top_of_a_hexagon()
+    make_quotes()
+    make_bottom_of_a_hexagon()
+    happy_birthday("Liana and Allison")
+    adding_numbers(4, 6)
+
+
+main()
+
+
