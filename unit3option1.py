e3d52b8abe54bdb1f7d226302072575172192493
ariana <ariana.daney@ssfs.org>
2019-10-02 09:43:16 -0400

    finished final unit 3 project that takes users width, length, and height to calculate the surface area of a rectangular prism

--- unit3option1.py
+++ unit3option1.py
@@ -4,43 +4,66 @@
 
 
 def instructions():
+    """
+    this function tells the user what the program does
+    :return: none
+    """
     print("this function will calculate the area of a rectangular prism")
     print("please enter width, length, and height")
 
 
 def user_width():
-    width = float(int("width:"))
-    return width
+    """
+    this function gets the users chosen width
+    :return: the users given width
+    """
+    return float(input("width:"))
 
 
 def user_length():
-    length = float(int("length:"))
-    return length
+    """
+    this function gets the users chosen length
+    :return: the users given length
+    """
+    return float(input("length:"))
 
 
 def user_height():
-    height = float(int("height:"))
-    return height
+    """
+    this function gets the users chosen height
+    :return: the users given height
+    """
+    return float(input("height:"))
 
 
 def area_of_rectangle(length, width):
-    area = length * width
-    return area
+    """
+    this function calculates the area of a rectangle
+    :param length: the length of the rectangle
+    :param width: the width of the rectangle
+    :return: the length times the width
+    """
+    return length * width
 
 
-def surface_area(height, length, width):
-    surface = height * length * width
-    return surface
+def surface_area(length, width, height):
+    """
+    this function calculates the total surface area of the rectangular prism
+    :param length: the length of the rectangle
+    :param width: the width of the rectangle
+    :param height: the height of the rectangle
+    :return: the sides times two, all added together
+    """
+    return (area_of_rectangle(length, width) * 2) + (area_of_rectangle(length, height) * 2) + \
+           (area_of_rectangle(width, height) * 2)
 
 
 def main():
     instructions()
-    area = user_length(), user_width()
-    surface = user_width(), user_length(), user_height()
-
-
-
-
+    width = user_width()
+    length = user_length()
+    height = user_height()
+    print("the total surface area is", surface_area(width, length, height))
 
 
 main()
