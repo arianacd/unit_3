4af88c0be4b7c7cb3cf0beb0faf26c0381287be5
ariana <ariana.daney@ssfs.org>
2019-10-02 10:06:29 -0400

    wrote "mississippi" in my own font

--- letters.py
+++ letters.py
@@ -3,6 +3,7 @@
 # creating a font
 
 import turtle
+turtle.speed(0)
 
 
 def invisible_move(num1, num2):
@@ -12,6 +13,7 @@ def invisible_move(num1, num2):
 
 
 def letter_m():
+    turtle.color("hot pink")
     turtle.seth(90)
     turtle.fd(50)
     turtle.right(135)
@@ -40,8 +42,15 @@ def letter_s():
     turtle.fd(28)
 
 
-def letter_p
-
+def letter_p():
+    turtle.seth(90)
+    turtle.fd(50)
+    turtle.right(90)
+    turtle.fd(28)
+    turtle.right(90)
+    turtle.fd(14)
+    turtle.right(90)
+    turtle.fd(28)
 
 
 def main():
@@ -52,6 +61,23 @@ def main():
     invisible_move(-328, 0)
     letter_s()
     invisible_move(-278, 0)
+    letter_s()
+    invisible_move(-228, 0)
+    letter_i()
+    invisible_move(-206, 0)
+    letter_s()
+    invisible_move(-156, 0)
+    letter_s()
+    invisible_move(-106, 0)
+    letter_i()
+    invisible_move(-84, 0)
+    letter_p()
+    invisible_move(-34, 0)
+    letter_p()
+    invisible_move(16, 0)
+    letter_i()
+    invisible_move(20, 0)
+    turtle.hideturtle()
 
 
 main()
